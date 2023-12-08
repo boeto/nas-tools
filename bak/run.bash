cc1204c66ac29ad6ab76663d2f8e77c67f6b5836
boeto <bo@eto.me>
2023-12-08 22:16:30 +0800

    remove bak

--- bak/run.bash
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-find . -type d -name "__pycache__" -exec rm -r {} +
-
-echo 'fs.inotify.max_user_watches=5242880' >>/etc/sysctl.conf &&
-    echo 'fs.inotify.max_user_instances=5242880' >>/etc/sysctl.conf &&
-    sudo sysctl -p
